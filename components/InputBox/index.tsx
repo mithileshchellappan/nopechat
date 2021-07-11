530d1c6eb3dc29d3a4b6e12383668d533f48ba04
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-07-11 16:23:34 +0530

    text input init

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import styles from './styles'
-const InputBox = ()=> {
-    return (
-        <View style={styles.container}>
-            <View style={styles.mainContainer}>
+import { Entypo, FontAwesome5, Fontisto, MaterialCommunityIcons } from "@expo/vector-icons";
+import React from "react";
+import { View,  TextInput } from "react-native";
+import styles from "./style";
+const InputBox = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.mainContainer}>
+        <FontAwesome5  name="laugh-beam"  size={24} color="grey" />
+        <TextInput keyboardType="default" multiline={true} style={styles.textInput}/>
+        <Entypo style={{marginRight:10}} name="attachment" size={24} color="grey"/>
+        <Fontisto name="camera" size={24} color="grey"/>
 
-            </View>
-            <View>
-                
-            </View>
-        </View>
-    )
-}
+      </View>
+      <View style={styles.buttonContainer}>
+        <MaterialCommunityIcons name="microphone" size={28} color="white" />
+      </View>
+    </View>
+  );
+};
 
-export default InputBox
-
+export default InputBox;
--- components/InputBox/style.ts
+++ components/InputBox/style.ts
@@ -1,7 +1,35 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    margin: 10,
+    alignItems:'baseline'
+  },
+  mainContainer: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 25,
+    alignItems: "baseline",
+    marginRight: 10,
+    flex: 1,
+    
+  },
+  textInput: {
+    flex: 1,
+    marginHorizontal:10
+  },
+  buttonContainer: {
+    alignItems: "center",
+    backgroundColor: Colors.light.tint,
+    borderRadius: 50,
+    justifyContent: "center",
+    width: 50,
+    height: 50,
 
-})
+  }
+});
 
-export default styles
+export default styles;
